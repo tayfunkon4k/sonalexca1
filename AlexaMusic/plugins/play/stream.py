1547a82e5d734300115f9d29c772f34936333486
TAYFUN <87475699+tayfunkon4k@users.noreply.github.com>
2025-01-08 23:15:51 +0300

    Update stream.py

--- AlexaMusic/plugins/play/stream.py
+++ AlexaMusic/plugins/play/stream.py
@@ -41,7 +41,7 @@ async def stream_command(
 ):
     if url:
         mystic = await message.reply_text(
-            _["play_2"].format(channel) if channel else _["play_1"]
+            _["oynat_2"].format(channel) if channel else _["oynat_1"]
         )
         try:
             await Alexa.stream_call(url)
