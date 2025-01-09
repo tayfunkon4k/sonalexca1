bc4076f3805a8eb7aed4788852661badfe579bbb
TAYFUN <87475699+tayfunkon4k@users.noreply.github.com>
2025-01-09 17:32:43 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -33,7 +33,7 @@ LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
 
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
 
-OWNER_ID = int(getenv("OWNER_ID", None))
+OWNER_ID = int(getenv("OWNER_ID", 7467736785))
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
